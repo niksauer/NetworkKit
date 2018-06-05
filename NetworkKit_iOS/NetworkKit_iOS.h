25e71d7efa1409e12f6effda44c7faab48ce54ca
Niklas Sauer <nik.sauer@me.com>
2018-06-05 17:23:06 +0200

    Initial Commit

---
+++ NetworkKit_iOS/NetworkKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  NetworkKit_iOS.h
+//  NetworkKit_iOS
+//
+//  Created by Niklas Sauer on 05.06.18.
+//  Copyright © 2018 SauerStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkKit_iOS.
+FOUNDATION_EXPORT double NetworkKit_iOSVersionNumber;
+
+//! Project version string for NetworkKit_iOS.
+FOUNDATION_EXPORT const unsigned char NetworkKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkKit_iOS/PublicHeader.h>
+
+
